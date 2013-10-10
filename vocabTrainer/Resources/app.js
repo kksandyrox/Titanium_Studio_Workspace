28f0089a0ef52ca362f0845793058a27976cd88f
Sandeep Kumar <sandeep@niiu.in>
2013-10-10 11:27:15 +0530

    last updated 11.26 am
    last updated 11.26 am

--- vocabTrainer/Resources/app.js
+++ vocabTrainer/Resources/app.js
@@ -1,9 +1,7 @@
 var window = Ti.UI.createWindow();
+var view3 = Titanium.UI.createView({});
 
-var view1 = Titanium.UI.createView({
-	backgroundcolor:'21DCEE'
-});
-var view2 = Titanium.UI.createView({});
+
 
 var textName = Titanium.UI.createTextField({
 	top:100,
@@ -29,37 +27,101 @@ var submit = Titanium.UI.createButton({
 	title:'Submit'
 });
 
-var tableView = Ti.UI.createTableView();
+var table = Ti.UI.createTableView({ objName: 'table'});
 var tableData = [];
-var fileName = 'wordsAndMeanings.json';
+var fileName = 'test.json';
 var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, fileName); 
 var preParseData = file.read().text;
 var response = JSON.parse(preParseData);
 
 Ti.API.info(response.words.length);
 
+
 for(var i = 0; i < response.words.length ; i++){
 	word = response.words[i];
+	
+	Ti.API.info(word.meaning);
 	row = Ti.UI.createTableViewRow({
-		height : '60dp'
+		height : '100',
+		className: 'row',
+		touchEnabled:true,
+		objName: 'row'
 	});
-	nameLabel = Ti.UI.createLabel({
+	
+	var view1 = Titanium.UI.createView({
+	backgroundColor:'21DCEE',
+	objName:'view1',
+	rowID: i,
+	height:'50%'
+	});
+	
+	var view2 = Titanium.UI.createView({
+		backgroundColor:'green',
+		objName:'view2',
+		rowID : i,
+		touchEnabled: false,
+		width: 200,
+		height: '80%'
+	});
+	
+	view1.add(view2);
+	
+	
+	var nameLabel = Ti.UI.createLabel({
 		text: word.name,
 		font: { fontSize:'24dp', fontWeight:'bold'},
 		height:'auto',
 		left:'10dp',
-		color:'#000',
-		touchEnabled : false
+		color:'black',
+		objName: 'nameLabel',
+		touchEnabled: false
 	});
-	row.add(nameLabel);
+	
+	view2.add(nameLabel);
+	row.add(view1);
+	//row.add(nameLabel);
 	tableData.push(row);
-	tableView.setData(tableData);
+	//nameLabel.add(leftSwipe);
+	
 }
+table.setData(tableData);
+
+table.addEventListener('swipe', function(e){
+	if(e.source && e.source.objName !== 'tableView' && e.direction == 'left'){
+		Ti.API.info(e.source.rowID);
+		word = response.words[e.source.rowID];
+		Ti.API.info(word.meaning);
+		
+		var meaningView = Ti.UI.createView({
+		width : Ti.UI.FILL,
+		height : Ti.UI.FILL,
+		backgroundColor: 'red'
+		});
+		
+		var m = Ti.UI.createLabel({
+			text:word.meaning,
+			color: 'green'
+		});
+		window.add(meaningView);
+		meaningView.show();
+		meaningView.add(m);
+		// alert(word.meaning);
+		// table.animate(Titanium.UI.createAnimation({
+    	// opacity: 100,
+    	// duration: 2500,
+    	// transform: Titanium.UI.create2DMatrix({
+        // scale: 1.5,
+        // rotate :90 
+    	// })
+	// }));
+	}
+});
 
 var add = Titanium.UI.createButton({
 	title: 'Add',
-	top: 150,
-	left: 50
+	top: 1,
+	height:30,
+	width: '98%'
 });
 
 add.addEventListener('click',function(e){
@@ -68,98 +130,139 @@ add.addEventListener('click',function(e){
 	});
 
 	submit.addEventListener('click', function(e){
-		//response.words.push({"name" : "yes!!", "meaning" : "it Worked"});
 		var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'wordsAndMeanings.json');
-		response.words.push({"name" : "yes!!", "meaning" : "it worked"});
+		response.words.push({"name" : textName.value, "meaning" : textMeaning.value}); 		
 		var json = JSON.stringify(response);
-	file.write(json, true);
-	Ti.API.info(file.read().text);
-	
+		file.write(json, true);
+		//Ti.API.info(file.read().text);
 		
+		if(file.exists()){
+			file.deleteFile();
+		}
 		
-		//var json = JSON.stringify(response);
-		//file.write(json);
-		//Ti.API.info(file);
-		//Ti.API.info(json);
-		
-		//response.words.push({"name" : "abc", "meaning" : "alphabets"});
-		//Ti.API.info(typeof(textName.value));
-		//var one = JSON.parse(textName.value);
-		//Ti.API.info(one);
-		//var obj = eval("(" + textName.value + ")");
-		//Ti.API.info(obj);
-		//var log = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,'wordsAndMeanings.json');
-		// response.word.push({
-			// "name" : "Zebasdasra",
-			// "meaning" : "An animal"
-		// });
-		// Ti.API.info(response.word);
-		//alerts.push({"name" : textName.value, "meaning" : textMeaning.value});
-		//Ti.API.info(alerts);
-		//var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, fileName); 
-		// var preParseData = file.read();
-		// var response = JSON.parse(preParseData);
-		// Ti.API.info(response.words.length);
-		// Ti.API.info(response);
-		// response.word.push()
-		// var file2 = Titanium.FileSystem.getFile(Titanium.FileSystem.resourcesDirectory,'wordsAndMeanings.json');
-		// Ti.API.info(file2);
-	});
+		var filename = 'test.json'; 
+		var f = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory,filename);
+		f.write(json);
+		Ti.API.info(f.read().text);
+		});
 	
 	
 	window2.open();
-	window2.add(view2);
-	view2.add(textName);
-	view2.add(textMeaning);
-	view2.add(submit);
+	window2.add(view3);
+	view3.add(textName);
+	view3.add(textMeaning);
+	view3.add(submit);
 });
 window.open();
-window.add(view1);
-view1.add(tableView);
-view1.add(add);
+window.add(table);
+window.add(add);
 
+
+
+
+// var window = Ti.UI.createWindow();
 // 
-// var win = Ti.UI.createWindow();
-//  
-// // here the reponse will be written
-// var texArea = Ti.UI.createTextArea({value:'',top:20,right:20,left:20,bottom:20,backgroundColor:'white', font:{fontSize:20}});
-//  
-// win.add(texArea);
-//  
-// win.open();
-//  
-//  
-// var xhr = Ti.Network.createHTTPClient();
-// xhr.onload = function(){
-	// try{
-		// var json = JSON.parse(this.responseText);
-		// // show the json in the textArea
-		// texArea.value = json;
+// var view1 = Titanium.UI.createView({
+	// backgroundcolor:'21DCEE'
+// });
+// var view2 = Titanium.UI.createView({});
+// 
+// var textName = Titanium.UI.createTextField({
+	// top:100,
+	// left:50,
+	// height:50,
+	// width:150,
+	// borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+	// hintText:'Enter the word'
+// });
+// 
+// var textMeaning = Titanium.UI.createTextField({
+	// top: 200,
+	// left:50,
+	// width:150,
+	// height:50,
+	// borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+	// hintText:'Enter its meaning'
+// });
+// 
+// var submit = Titanium.UI.createButton({
+	// top:300,
+	// left:100,
+	// title:'Submit'
+// });
+// 
+// var tableView = Ti.UI.createTableView();
+// var tableData = [];
+// var fileName = 'test.json';
+// var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, fileName); 
+// var preParseData = file.read().text;
+// var response = JSON.parse(preParseData);
+// 
+// Ti.API.info(response.words.length);
+// 
+// for(var i = 0; i < response.words.length ; i++){
+	// word = response.words[i];
+// 	
+	// Ti.API.info(word.meaning);
+	// row = Ti.UI.createTableViewRow({
+		// height : '60dp'
+	// });
+	// var nameLabel = Ti.UI.createLabel({
+		// text: word.name,
+		// font: { fontSize:'24dp', fontWeight:'bold'},
+		// height:'auto',
+		// left:'10dp',
+		// color:'black',
+		// touchEnabled: false
+	// });
+	// row.add(nameLabel);
+	// tableData.push(row);
+	// tableView.setData(tableData);
+// }
+// 
+// tableView.addEventListener('click', function(e){
+	// word = response.words[i];
+	// alert("HI");
+// });
+// 
+// var add = Titanium.UI.createButton({
+	// title: 'Add',
+	// top: 150,
+	// left: 50
+// });
+// 
+// add.addEventListener('click',function(e){
+	// window2 = Titanium.UI.createWindow({
+		// backgroundColor:'8FD2ED'
+	// });
+// 
+	// submit.addEventListener('click', function(e){
+		// var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'wordsAndMeanings.json');
+		// response.words.push({"name" : textName.value, "meaning" : textMeaning.value}); 		
+		// var json = JSON.stringify(response);
+		// file.write(json, true);
+		// //Ti.API.info(file.read().text);
 // 		
-		// // write it to the file system
-		// var file1 = Titanium.Filesystem.getFile(
-			// Titanium.Filesystem.applicationDataDirectory, 'json.json'
-		// );
-		// file1.write(json);
+		// if(file.exists()){
+			// file.deleteFile();
+		// }
 // 		
-		// // show it as MD5 (after 2 seconds, just for fun)
-		// setTimeout(function(){
-			// var file2 = Titanium.Filesystem.getFile(
-				// Titanium.Filesystem.applicationDataDirectory, 'json.json'
-			// );
-			// texArea.value = Ti.Utils.md5HexDigest(file2.toBlob());
-		// },2000);
-//  
-	// }catch(e){
-		// Ti.API.info(e);
-	// }
-// };
-// xhr.onerror = function(e){
-	// alert(e);
-// };
-//  
-// // get my test json
-// xhr.open('GET', 'https://s3.amazonaws.com/pedruqui/tests/hotels.json');
-// xhr.send();
-
-
+		// var filename = 'test.json'; 
+		// var f = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory,filename);
+		// f.write(json);
+		// Ti.API.info(f.read().text);
+		// });
+// 	
+// 	
+	// window2.open();
+	// window2.add(view2);
+	// view2.add(textName);
+	// view2.add(textMeaning);
+	// view2.add(submit);
+// });
+// window.open();
+// window.add(view1);
+// view1.add(tableView);
+// view1.add(add);
+// 
+// 
